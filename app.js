468c3b9db7a30d416ad1e93a8e08f5b1076aa0b3
Marcos Paulo de Oliveira Barroso <marcos.barroso@iesb.br>
2019-04-09 19:13:47 -0300

    emitter

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 var http = require('http');
 var handle = require('./handle');
-var name = require('./name');
+//var name = require('./name');
 
-console.log(name);
+var events = require('events');
+var emiter = new events.EventEmitter();
 
+/*console.log(name);
+
+var server = http.createServer(handle.fn);
+
+emiter.on('say12',say);
+
+function say(){
+	console.log('Im saing...');
+}*/
 var server = http.createServer(handle.fn);
 
 server.listen(3000, function(){
--- file.js
+++ file.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 
 //fs = file system
 
+//escrever arquivo
 /*fs	
 	.writeFile('data.txt','Hello World', function(err){
 		if(err){
@@ -10,12 +11,36 @@ var fs = require('fs');
 	})*/
 
 
-fs
+//ler arquivo 
+/*fs
 	.readFile('data.txt',function(err, data){
 		if(err){
 			throw err;
 		}
 
 		console.log(data.toString('utf8'));
-	})
+	})*/
 
+
+//listar arquivos do diretório "."
+/*	fs
+		.readdir('.',function(err,files){
+			if(err){
+				throw err;
+			}
+
+			for(var file in files){
+				console.log(files[file]);
+			}
+		})*/
+
+//listar arquivos ".js"
+	fs
+
+		.readdirSync('.')
+		.filter(function(file){
+			return (file.endsWith('.js'))
+		})
+		.forEach(function(file){
+			console.log(file)
+		})
